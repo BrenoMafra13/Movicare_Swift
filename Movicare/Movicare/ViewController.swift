0fa4e49c04719e67e4671a0d48fb67c77e128b6b
BrenoMafra13 <brenolopesmafra@gmail.com>
2026-01-30 08:48:37 -0500

    Add first project structure

---
+++ Movicare/Movicare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movicare
+//
+//  Created by Breno Lopes Mafra on 2026-01-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
