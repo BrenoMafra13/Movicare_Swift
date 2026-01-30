19b578a92fd756b7b747fb815132c8b250b7e728
BrenoMafra13 <brenolopesmafra@gmail.com>
2026-01-30 11:14:42 -0500

    Fixing structure

---
+++ Movicare/Movicare/MovicareApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovicareApp.swift
+//  Movicare
+//
+//  Created by Breno Lopes Mafra on 2026-01-30.
+//
+
+import SwiftUI
+
+@main
+struct MovicareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
