4b7f1e27177ee64c4a7ad48f44cf01d02fb7a925
BrenoMafra13 <brenolopesmafra@gmail.com>
2026-02-01 09:31:37 -0500

    Adding flow between screens

---
+++ Movicare/Movicare/Utils.swift
@@ -0,0 +1,18 @@
+//
+//  Utils.swift
+//  Movicare
+//
+//  Created by Breno Lopes Mafra on 2026-02-01.
+//
+
+import SwiftUI
+
+struct Utils: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Utils()
+}
